c90aea3182c154eccf38ff000b0f5fa6a320b845
magdy_edwar <magdyedwar1996@gmail.com>
2021-07-10 00:07:22 +0200

    this is my solution for this problem

---
+++ 00001_____Challenges/02_HackerRank/03_In Python/02_Python If-Else/Python_else_if.py
@@ -0,0 +1,14 @@
+n = input()
+n = int(n)
+if (n %2 != 0):
+    print("Weird")
+elif ((n %2 == 0) and (2< n) and (n<5)):
+    print("Not Weird")
+
+elif ((n %2 == 0) and (2<= n) and (n<=20)): 
+    print("Weird")
+
+else :
+    print("Not Weird")
+    
+     
