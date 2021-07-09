df56995bac07d004947eb47181ffa29b52a1b165
magdy_edwar <magdyedwar1996@gmail.com>
2021-07-09 13:02:30 +0200

    my solution for this problem

---
+++ 00001_____Challenges/02_HackerRank/01_In C language/06_For Loop in C/The_For_Loop_Solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+
+int main() 
+{
+    int a, b;
+    scanf("%d\n%d", &a, &b);
+    for (int i = a ; i<=b ; i++)
+    {
+        if ((i>1)&&(i<=9))
+        {
+        
+        }
+    }
+    return 0;
+}
+
