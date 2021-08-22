11b1823374089dfc86611b65719348d012834872
Magdy Edwar <magdiedwar47@gmail.com>
2021-08-22 16:59:00 +0200

    Editing the code to be more reliable

--- 00001_____Challenges/02_HackerRank/01_In C language/12_Students Marks Sum/Students_marks_sum.c
+++ 00001_____Challenges/02_HackerRank/01_In C language/12_Students Marks Sum/Students_marks_sum.c
@@ -1,47 +1,43 @@
 #include <stdio.h>
+#include <string.h>
+#include <math.h>
 #include <stdlib.h>
-int marks_summation(int* marks, int number_of_students, char gender);
- int sum = 0 ;
-int main ()
+
+int marks_summation(int* marks ,int  number_of_students,char gender)
 {
-	int number_of_students ; 
-	char gender ; 
-	scanf("%d",&number_of_students);
-	int *marks = (int*) malloc(sizeof(int)*number_of_students);
-	for (int i= 0 ; i<number_of_students ; i++)
-		{
-			scanf("%d",&marks);
-			marks++;
-		}
-	scanf("\n");	
-	scanf("%c",&gender);
-	printf("%d",marks_summation(marks ,number_of_students ,gender ));
-	return 0 ; 
+    int i , sum = 0 ; 
+    if (gender == 'b')
+        {
+            i =  0;
+        }
+    else
+        {
+        i = 1 ;  
+        }
+    
+    for (;i <number_of_students ; i=i+2)
+    {
+        sum = sum + marks[i];
+    }
+    return sum ; 
 }
 
-////////////////////////////////////////////////////////////////////////
-////////////////////////////////////////////////////////////////////////
-
-int marks_summation(int* marks, int number_of_students, char gender)
- {
-	
-    if (gender == 'b')
-		{
-			for (int i =0 ; i<number_of_students ; )
-			{
-				sum +=marks[i];	
-				i+=2;
-			}	
-		}
-	else if (gender == 'g')
-		{
-			for (int i =1 ; i<number_of_students ;)
-			{
-				sum +=marks[i];	
-				i+=2;
-			}
-		}
-	  
-  return sum ;
-   
+int main() {
+    int number_of_students;
+    char gender;
+    int sum;
+  
+    scanf("%d", &number_of_students);
+    int *marks = (int *) malloc(number_of_students * sizeof (int));
+ 
+    for (int student = 0; student < number_of_students; student++) {
+        scanf("%d", (marks + student));
+    }
+    
+    scanf(" %c", &gender);
+    sum = marks_summation(marks, number_of_students, gender);
+    printf("%d", sum);
+    free(marks);
+ 
+    return 0;
 }
