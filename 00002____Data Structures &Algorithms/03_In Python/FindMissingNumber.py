d4576036826ae85e384b241b5ff00743e35d4825
Magdy Edwar <magdiedwar47@gmail.com>
2022-08-01 09:20:38 +0200

    some changes in the ordering of the files

--- 00002____Data Structures &Algorithms/03_In Python/FindMissingNumber.py
+++ 00002____Data Structures &Algorithms/03_In Python/FindMissingNumber.py
@@ -1,13 +1,20 @@
 # Find the missing number
 
-array = [0,1,2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 15, 16]
+array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, 13, 14, 16, 17, 18, 20, 22,25]
 
 
 def FindMissingNumber(array):
+    """This is the first way to find the missing element"""
+    # for i in range(array[0], array[-1]):
+    #     if array[i] != array[i + 1] - 1:
+    #         return array[i] + 1
+    #
+
+    """ this is the second way to find the missing element """
     for i in range(array[0], array[-1]):
-        if array[i] != array[i + 1] - 1:
-            return array[i] + 1
+        if i not in array:
+            print(f"Found element :{i}")
 
 
-Missing_Element = FindMissingNumber(array)
-print(f"This is the missing elemetn in this array ", Missing_Element)
+FindMissingNumber(array)
+# print(f"This is the missing element in this array ", FindMissingNumber(array))
